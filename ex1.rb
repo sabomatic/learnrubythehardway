3d98caf175c6d11e62d500fb9711d429ee61a1c7
Bootcamp <bootcamp@Sab.local>
2017-01-12 11:23:00 +0100

    Finish excercise 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World"
+puts "Hello again"
+puts "I like typing this"
+puts "This is fun!"
+puts "yay printing!"
+puts "I'd much rather you 'not'"
+puts 'I "said" do not touch this.'
+puts "two plus two equals #{2+2}"
+puts 'It\'s a lovely day outside'
