d7e329e898257e96a1d84e079770e20c787ad19b
Bootcamp <bootcamp@Sab.local>
2017-01-12 11:33:27 +0100

    Finish exercise 7

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb." # print text
+puts "Its fleece was white as #{'snow'}" # print text
+puts "And everywhere that Mary went." # print text
+puts "." * 10 # what'd that do? put 10 dots
+
+end1 = "C" # assign letter to variable
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs puts:  print put text on the following line?
+print end1 + end2 + end3 + end4 + end5 + end6 # all this text will go on the next line
+puts end7 + end8 + end9 + end10 + end11 + end12 # this text will stay here
