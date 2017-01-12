2b18301eb5b9cc9c9e8988dd9808897e322208af
Bootcamp <bootcamp@Sab.local>
2017-01-12 11:27:05 +0100

    Finish excercise 2

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# A comment, this is so you can read that program later
+# Anything after the # is ignored by ruby.
+
+puts "I could have code like this." # and the comment after is ignored
+# you can also use # to disable a line of code
+# puts "This won't run."
+
+puts "This will run"
