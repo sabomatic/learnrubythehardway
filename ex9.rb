c441d831a011ce8ed83220f1e71e6f2b69061baa
Bootcamp <bootcamp@Sab.local>
2017-01-12 11:36:22 +0100

    Finish exercise 9

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+# new strange stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun" # this is assigning the string of days to "days"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # this is assigning the string of months to "months"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+  There's something going on here,
+  With the three double-quotes.
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5 or 6.
+}
+# the %q{} is allowing us to type lines without having to use puts for each line.
