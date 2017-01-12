90f5e57557cc22ec3e14c3db4c0bbce48bdb4e66
Bootcamp <bootcamp@Sab.local>
2017-01-12 11:32:56 +0100

    Finish exercise 6

---
+++ ex6.rb
@@ -0,0 +1,28 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'"
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side"
+
+puts w + e
+
+# there are 10 types of people
+# those who know binary and those who don't
+# I said: there are 10 types of people
+# I also said: those who know binary and those who don't
+# Isn't that joke so funny?! false
+# This is the left side of... a string with a right side
