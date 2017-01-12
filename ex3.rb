c2661b8835fc50419e059cf975a986290f5905d6
Bootcamp <bootcamp@Sab.local>
2017-01-12 11:27:56 +0100

    Finish exercise 3

---
+++ ex3.rb
@@ -0,0 +1,34 @@
+# text
+puts "I will now count my chickens:"
+
+# compute number of all chickens
+puts "Hens #{25.0+30.0/6.0}"
+puts "Roosters #{100.0-25.0*3.0%4.0}"
+
+# text
+puts "Now I will count the eggs:\n"
+
+# compute all eggs
+puts 3+2+1-5+4%2-1/4+6
+puts ""
+
+# text
+puts "Is it true that 3+2 < 5-7?"
+
+# text
+puts 3+2 < 5-7
+
+# compute individual calculations
+puts "What is 3+2? #{3+2}"
+puts "What is 5-7? #{5-7}"
+
+# text
+puts "Oh, that's why it's false"
+
+puts "How about some more"
+
+
+# logic test
+puts "Is it greater? #{5> -2}"
+puts "Is it greater of equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
